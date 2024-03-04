72020ee4b0552cb8d3c650286cbd0afa6880b2bb
Tejas Patade <tej.patade69@gmail.com>
2024-03-04 10:22:26 +0530

    Validation for prompt

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,3 +1,4 @@
+// import axios from "axios";
 import { useState } from "react";
 import { FaArrowDown } from "react-icons/fa6";
 
@@ -5,36 +6,45 @@ import Results from "./Results";
 
 const Prompt = () => {
   const [prompt, setPrompt] = useState<string>("");
+  const [isValidPrompt, setIsValidPrompt] = useState<boolean>(true);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(e.target.value);
+
+    // Reset validation when user starts typing
+    setIsValidPrompt(true);
   };
 
   const handleSubmit = () => {
-    // Forming the prompt data
-    const promptData = {
-      prompt: prompt,
-      time: new Date().toISOString()
+    if (prompt.trim() === "") {
+      // If input value is empty, set isValid to false
+      setIsValidPrompt(false);
+    } else {
+      // Forming the prompt data
+      const promptData = {
+        prompt: prompt,
+        time: new Date().toISOString()
+      }
+      console.log(promptData);
+
+      // Clearing Prompt after submission
+      setPrompt("");
+
+      // Prompt goes to backend
+      // axios.post("http://localhost:5000/prompt", promptData)
     }
-    console.log(promptData);
-
-    // Clearing Prompt after submission
-    setPrompt("");
-
-    // Prompt goes to backend
-  
-
   };
 
   return (
     <section className="bg-[#fafaf7] mt-4 mx-4 p-16">
       <h1 className="text-xl font-semibold mb-4">Explain how you would like the House Plan to look</h1>
+      {!isValidPrompt && <p className="text-red-500 text-xs">Please enter a valid prompt</p>}
       <div className="flex gap-2 mb-6">
         <input
           type="text"
           value={prompt}
           onChange={handleInputChange}
-          className="border border-gray-300 rounded-md h-9 px-4 py-2 mb-4 w-screen"
+          className={` border ${isValidPrompt ? "border-gray-300" : "border-red-500"} rounded-md h-9 px-4 py-2 mb-4 w-screen`}
           placeholder="Describe what you're looking for..."
           />
         <button
