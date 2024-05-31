07fe2efa97a01dd0971cefcdc1547c31ff47f86e
Tejas Patade <tej.patade69@gmail.com>
2024-05-31 09:31:25 +0530

    Image relative path added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = ({page, setPage} : NavbarProps) => {
   return (
     <nav className="bg-[#f7f6f1] p-2 mt-4 mx-4 flex justify-between items-center">
       <div className="flex items-center">
-        <img src="Logo.png" alt="Brand Logo" className="h-16 mr-2" />
+        <img src="/Logo.png" alt="Brand Logo" className="h-16 mr-2" />
         <span className="font-bold text-lg select-none">Floorigami</span>
       </div>
 
